8cef459e273e997b4342b1c413b90beca4dfa067
MarkoSagadin <marko.sagadin42@gmail.com>
2022-10-05 11:36:01 +0200

    Add extra args to flash and build command

--- src/east/workspace_commands/basic_commands.py
+++ src/east/workspace_commands/basic_commands.py
@@ -21,6 +21,7 @@ def clean(east):
 @click.option("-b", "--board", type=str, help="West board to build for.")
 @click.option("-d", "--build-dir", type=str, help="Build directory to create or use.")
 @click.option("-t", "--target", type=str, help="Run this build system target.")
+@click.argument("cmake-args", nargs=-1, type=str, metavar="-- [cmake-args]")
 @click.option(
     "-s",
     "--source-dir",
@@ -31,11 +32,22 @@ def clean(east):
     ),
 )
 @click.pass_obj
-def build(east, board, build_dir, target, source_dir):
+def build(east, board, build_dir, target, source_dir, cmake_args):
     """
     Build firmware in current directory.
 
     Internally runs [magenta bold]west build[/] command in current directory if --source-dir is set. If the --build-dir directory is not set, the default is build/ unless the build.dir-fmt configuration variable is set. The current directory is checked after that. If either is a Zephyr build directory, it is used.
+
+    \n\nTo pass additional arguments to the [bold]CMake[/] invocation performed by the [magenta
+    bold]west build[/], pass them after a [bold white]"--"[/] at the end of the command line.
+
+    \n\n[bold]Important:[/] Passing additional [bold]CMake[/] arguments like this forces [magenta
+    bold]west build[/] to re-run [bold]CMake[/], even if a build system has already been generated.
+
+    For additional info see chapter [bold]Building, Flashing and Debugging[/], section
+    [bold]One-Time CMake Arguments[/].
+
+
 
     \n\n[bold]Note:[/] This command can be only run from inside of a [bold yellow]West workspace[/].
     """
@@ -52,6 +64,8 @@ def build(east, board, build_dir, target, source_dir):
         build_cmd += f"-t {target} "
     if source_dir:
         build_cmd += f"{source_dir} "
+    if cmake_args:
+        build_cmd += f"-- \"{' '.join(cmake_args)}\" "
 
     east.run_west(build_cmd)
 
@@ -78,12 +92,16 @@ def build(east, board, build_dir, target, source_dir):
         "Identification number of a JLink programmer that should be used for flashing."
     ),
 )
+@click.argument("extra-args", nargs=-1, type=str, metavar="-- [extra args]")
 @click.pass_obj
-def flash(east, build_dir, runner, verify, jlink_id):
+def flash(east, build_dir, runner, verify, jlink_id, extra_args):
     """
     Flash binary to the board's flash.
 
     Internally runs [magenta bold]west flash[/] command. If the build directory is not given, the default is build/ unless the build.dir-fmt configuration variable is set. The current directory is checked after that. If either is a Zephyr build directory, it is used. If there are more than one JLinks connected to the host machine use --jlink-id flag to specify which one to use to avoid selection prompt.
+
+    \n\nTo pass additional arguments to the [bold cyan]runner[/] used by the [magenta
+    bold]west flash[/], pass them after a [bold white]"--"[/] at the end of the command line.
 
 
 
@@ -99,12 +117,14 @@ def flash(east, build_dir, runner, verify, jlink_id):
         flash_cmd += f"-r {runner} "
 
     # Some flags need to be passed as extra parameters to the west tool
-    if verify or jlink_id:
+    if verify or jlink_id or extra_args:
         flash_cmd += "-- "
 
     if verify:
-        flash_cmd += "--verify"
+        flash_cmd += "--verify "
     if jlink_id:
-        flash_cmd += f"-i {jlink_id}"
+        flash_cmd += f"-i {jlink_id} "
+    if extra_args:
+        flash_cmd += f"\"{' '.join(extra_args)}\" "
 
     east.run_west(flash_cmd)
