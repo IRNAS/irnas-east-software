d8004489c403b9443572e00c1a041621c0bdb131
MarkoSagadin <marko.sagadin42@gmail.com>
2022-10-25 10:00:02 +0200

    Add first parametrized test

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,6 +1,13 @@
 import os
 
+import pytest
+from click.testing import CliRunner
+
+from east.__main__ import cli
 from east.east_context import EastContext
+from east.helper_functions import east_yml_not_found_msg
+
+from . import helpers
 
 
 def assert_all_is_none(east, path):
@@ -18,6 +25,13 @@ def test_good_west_workplace(west_workplace):
     assert east.cwd == project_path
     assert east.west_dir_path == os.path.dirname(project_path)
     assert east.detected_ncs_version == "v2.1.0"
+
+    assert east.ncs_version_installed is False
+    assert east.east_yml is None
+
+    east.pre_workspace_command_check()
+    assert east.ncs_version_installed is True
+    assert east.east_yml is not None
 
 
 def test_no_config_west_workplace(no_config_west_workplace):
@@ -45,3 +59,24 @@ def test_no_west_yaml_west_workplace(no_westyaml_west_workplace):
     project_path = no_westyaml_west_workplace
     east = EastContext()
     assert_all_is_none(east, project_path)
+
+
+list_of_workspace_commands = [
+    "build",
+    "flash",
+    "clean",
+    "bypass",
+    "release",
+]
+
+
+@pytest.mark.parametrize("workspace_command", list_of_workspace_commands)
+def test_no_east_yaml_west_workplace(no_eastyaml_west_workplace, workspace_command):
+    runner = CliRunner()
+
+    # Detection of east.yml needs to be done in every west_workplace command, clean is
+    # used here as an example
+    result = runner.invoke(cli, [workspace_command])
+
+    assert result.exit_code == 1
+    helpers.assert_strings_equal(result.output, east_yml_not_found_msg)
