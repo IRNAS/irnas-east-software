e2166e26d6b93414aba6c070953871ac8abf1b7d
MarkoSagadin <marko.sagadin42@gmail.com>
2023-09-21 13:03:49 +0200

    Add wrapper for west init command

--- src/east/__main__.py
+++ src/east/__main__.py
@@ -2,7 +2,7 @@ import click
 import rich_click
 
 from .east_context import EastContext, east_group_settings
-from .system_commands import sys_setup, util
+from .system_commands import init, sys_setup, util
 from .workspace_commands import (
     attach,
     build,
@@ -39,7 +39,7 @@ rich_click.rich_click.COMMAND_GROUPS = {
         },
         {
             "name": "System commands",
-            "commands": ["sys-setup", "util"],
+            "commands": ["init", "sys-setup", "util"],
         },
     ],
     "east update": [{"name": "Subcommands", "commands": ["west", "env", "toolchain"]}],
@@ -100,6 +100,7 @@ cli.add_command(bypass)
 cli.add_command(flash)
 cli.add_command(clean)
 cli.add_command(update)
+cli.add_command(init)
 cli.add_command(sys_setup)
 cli.add_command(util)
 cli.add_command(release)
--- src/east/system_commands/__init__.py
+++ src/east/system_commands/__init__.py
@@ -1,2 +1,3 @@
+from .basic_west_commands import init
 from .sys_setup import sys_setup
 from .util_commands import util
