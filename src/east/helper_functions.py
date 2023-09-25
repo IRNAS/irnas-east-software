IRNAS/irnas-east-software

commit 81bcb75ee0634b65ca3715ce5ce4263f70bcdfa1

Add more CodeChecker commands
Commands 'store', 'bypass' and 'servdiff' were added.

To store some information about the build process, the build command now
also creates a file in the build directory that is picked up by the
various CodeChecker commands.

src/east/workspace_commands/codechecker_helpers.py
@@ -5,6 +5,8 @@ import os
 import plistlib as plist
 import re
 
+from ..helper_functions import get_git_version
+
 
 def check_for_build_folder(east):
     """Check that build dir exists."""
@@ -240,3 +242,62 @@ def create_skip_file(east, output):
             f.write(f"-{east.west_dir_path}/*\n ")
 
     return skip_file
+
+
+def create_codecheckerfile(east, board, build_type, build_dir, source_dir):
+    """Create a file in the build folder that contains all information needed for the
+    codechecker store command.
+
+    The intention is that east codechecker store reads the file and uses it for
+    creating the metadata of the run when storing to the server.
+    """
+
+    # Create a file in the build folder that contains the name of the artefact
+    filename = os.path.join(build_dir if build_dir else "build", "codecheckerfile.json")
+    name = os.path.basename(source_dir if source_dir else east.cwd)
+    version = get_git_version(east)
+
+    data = {
+        "name": name,
+        "board": board,
+        "build_type": build_type,
+        "version": version,
+    }
+
+    with open(filename, "w") as f:
+        json.dump(data, f)
+
+
+def get_metadata_from_codecheckerfile(east):
+    """Read the codecheckerfile.json file and construct the name and the tag, intended
+    to be used by codechecker store command."""
+
+    # WARN: We are blindly assuming user used the default build "build" folder location.
+
+    filename = os.path.join("build", "codecheckerfile.json")
+    with open(filename, "r") as f:
+        data = json.load(f)
+
+    build_type = data["build_type"]
+    build_type = "" if build_type == "release" or not build_type else f"-{build_type}"
+
+    name = f"{data['name']}-{data['board']}{build_type}"
+
+    tag = f"tag: {data['version']['tag']}"
+
+    if data["version"]["hash"]:
+        tag += f", commit: {data['version']['hash']}"
+
+    return name, tag
+
+
+def get_endpoint(east):
+
+    endpoint = (
+        east.run("git config --get remote.origin.url", return_output=True, silent=True)[
+            "output"
+        ]
+        .split("/")[-1]
+        .strip()
+    )
+    return endpoint

src/east/__main__.py
@@ -45,7 +45,17 @@ rich_click.rich_click.COMMAND_GROUPS = {
     "east update": [{"name": "Subcommands", "commands": ["west", "env", "toolchain"]}],
     "east util": [{"name": "Subcommands", "commands": ["connect", "rtt"]}],
     "east codechecker": [
-        {"name": "Subcommands", "commands": ["check", "fixit", "example-config"]}
+        {
+            "name": "Subcommands",
+            "commands": [
+                "check",
+                "fixit",
+                "example-config",
+                "store",
+                "servdiff",
+                "bypass",
+            ],
+        }
     ],
 }
 

src/east/workspace_commands/basic_commands.py
@@ -9,6 +9,7 @@ from rich_click import RichCommand
 from ..east_context import east_command_settings
 from ..helper_functions import clean_up_extra_args
 from .build_type_flag import construct_extra_cmake_arguments
+from .codechecker_helpers import create_codecheckerfile
 
 
 @click.command(**east_command_settings)
@@ -134,6 +135,8 @@ def build(ctx, east, build_type, extra_help, args):
         sh.copyfile(
             compile_file, os.path.join(east.west_dir_path, "compile_commands.json")
         )
+
+    create_codecheckerfile(east, opts.board, build_type, opts.build_dir, source_dir)
 
 
 def create_build_command(

src/east/workspace_commands/codechecker_commands.py
@@ -1,9 +1,11 @@
 import os
+import shutil
 
 import click
 from rich.syntax import Syntax
 
 from ..east_context import east_command_settings, east_group_settings
+from ..helper_functions import clean_up_extra_args
 from .codechecker_helpers import (
     check_for_build_folder,
     check_for_codechecker_config_yaml,
@@ -11,9 +13,22 @@ from .codechecker_helpers import (
     cleanup_compile_commands_json,
     cleanup_plist_files,
     create_skip_file,
+    get_endpoint,
+    get_metadata_from_codecheckerfile,
 )
 
 CC_OUTPUT_DIR = os.path.join("build", "codechecker")
+CC_DIFF_OUTPUT_DIR = os.path.join("build", "codechecker_diff")
+
+# Print output of the parse and diff command,s with default rich highlighting, but without any
+# other wrapping, cropping.
+clean_print_args = {
+    "highlight": True,
+    "overflow": "ignore",
+    "crop": False,
+    "soft_wrap": False,
+    "no_wrap": True,
+}
 
 
 @click.command(**east_command_settings)
@@ -119,16 +134,7 @@ def check(east, html, dont_cleanup_plist, skip_file, file, only_analyze):
 
     result = east.run(parse_cmd, exit_on_error=False, return_output=True, silent=True)
 
-    # Print output of the parse command, with default rich highlighting, but without any
-    # other wrapping, cropping.
-    print_args = {
-        "highlight": True,
-        "overflow": "ignore",
-        "crop": False,
-        "soft_wrap": False,
-        "no_wrap": True,
-    }
-    east.print(result["output"], **print_args)
+    east.print(result["output"], **clean_print_args)
 
 
 @click.option(
@@ -179,7 +185,45 @@ analyzer:
 parse:
   - --trim-path-prefix=/*/project
   - --print-steps
+
+store:
+  - --trim-path-prefix=/*/project
 """
+
+
+@click.option(
+    "--url",
+    type=str,
+    default=lambda: os.getenv("EAST_CODECHECKER_SERVER_URL"),
+    help="URL of the Codechecker server (port number is also required). "
+    "If not explicitly given then value is read from the EAST_CODECHECKER_SERVER_URL "
+    "env var.",
+)
+@click.command(**east_command_settings)
+@click.pass_obj
+def store(east, url):
+    """Store the results of the [magenta bold]Codechecker[/] analysis to a server.
+
+    \b
+    \n\n[bold]Note:[/] This command should be ran after the [bold cyan]east codechecker check[/] command.
+
+    \n\n[bold]Note:[/] This command can be only run from inside of a [bold yellow]West workspace[/].
+    """
+
+    east.pre_workspace_command_check(check_only_west_workspace=True)
+
+    cc = east.consts["codechecker_path"]
+    cfg = os.path.join(east.project_dir, "codechecker_config.yaml")
+
+    name, tag = get_metadata_from_codecheckerfile(east)
+    endpoint = get_endpoint(east)
+
+    store_cmd = (
+        f"{cc} store --name '{name}' --url {url}/{endpoint} "
+        f"--config {cfg} {CC_OUTPUT_DIR} --tag '{tag}'"
+    )
+
+    east.run(store_cmd)
 
 
 @click.option(
@@ -215,6 +259,129 @@ def example_config(east, create):
     east.print(syntax)
 
 
+@click.command(
+    **east_command_settings,
+    context_settings=dict(ignore_unknown_options=True, allow_extra_args=True),
+)
+@click.pass_obj
+@click.option(
+    "--extra-help",
+    is_flag=True,
+    help="Print help of the given [bold magenta]Codechecker[/] command.",
+)
+@click.argument("args", nargs=-1, type=click.UNPROCESSED, metavar="")
+def bypass(east, extra_help, args):
+    """
+    Directly run any [magenta bold]CodeChecker[/] command.
+
+    \b
+    \n\nInternally runs [magenta bold]codechecker[/] command, all given arguments are passed directly to it.
+
+    \n\nTo learn more about possible [magenta bold]codechecker[/] arguments and options use --extra-help flag.
+
+
+    \n\n[bold]Note:[/] This command can be only run from inside of a [bold yellow]West workspace[/].
+    """
+
+    east.pre_workspace_command_check(check_only_west_workspace=True)
+
+    cc = east.consts["codechecker_path"]
+
+    cmd = f"{cc} "
+
+    if args:
+        cmd += f"{clean_up_extra_args(args)} "
+
+    if extra_help:
+        cmd += "--help"
+
+    east.run(cmd)
+
+
+@click.option(
+    "--new",
+    is_flag=True,
+    help="Show results that don't exist in the last server analysis but exist in the "
+    "local one. These are new issues that have arisen since the last store to the "
+    "server.",
+)
+@click.option(
+    "--resolved",
+    is_flag=True,
+    help="Show results that exist in the server analysis but aren't present in the "
+    "local one. These are issues that have been resolved since the last store "
+    "to the server.",
+)
+@click.option(
+    "--unresolved",
+    is_flag=True,
+    help="Show results that appear both in the last server analysis and in the "
+    "local analysis. These are issues that were present when last stored on the "
+    "server and are still present locally.",
+)
+@click.option(
+    "--html",
+    is_flag=True,
+    help="Generate a html report instead of printing the results in the terminal. "
+    "Default: false.",
+)
+@click.option(
+    "--url",
+    type=str,
+    default=lambda: os.getenv("EAST_CODECHECKER_SERVER_URL"),
+    help="URL of the Codechecker server (port number is also required). "
+    "If not explicitly given then value is read from the EAST_CODECHECKER_SERVER_URL "
+    "env var.",
+)
+@click.command(**east_command_settings)
+@click.pass_obj
+def servdiff(east, new, resolved, unresolved, html, url):
+    """Compare local analysis against the last server analysis.
+
+    \b
+    \n\nUse one of the --new, --resolved, --unresolved flags to specify how to compare.
+
+    \n\n[bold]Note:[/] This command should be ran after the [bold cyan]east codechecker check[/] command.
+
+    \n\n[bold]Note:[/] This command can be only run from inside of a [bold yellow]West workspace[/].
+    """
+
+    east.pre_workspace_command_check(check_only_west_workspace=True)
+
+    cc = east.consts["codechecker_path"]
+
+    name, _ = get_metadata_from_codecheckerfile(east)
+    endpoint = get_endpoint(east)
+
+    diff_cmd = (
+        f"{cc} cmd diff --basename {name} --newname {CC_OUTPUT_DIR} "
+        f"--url {url}/{endpoint} "
+    )
+
+    if [new, resolved, unresolved].count(True) != 1:
+        east.print(
+            "\nExactly one of the [bold cyan]--new[/], [bold cyan]--resolved[/], [bold cyan]--unresolved[/] flags must be given."
+        )
+        east.exit()
+
+    if new:
+        diff_cmd += "--new "
+
+    if resolved:
+        diff_cmd += "--resolved "
+
+    if unresolved:
+        diff_cmd += "--unresolved "
+
+    if html:
+        shutil.rmtree(CC_DIFF_OUTPUT_DIR, ignore_errors=True)
+        diff_cmd += f"--export {CC_DIFF_OUTPUT_DIR} --output html "
+
+    result = east.run(diff_cmd, exit_on_error=False, return_output=True, silent=True)
+
+    east.print(result["output"], **clean_print_args)
+
+
 @click.group(**east_group_settings, subcommand_metavar="Subcommands")
 @click.pass_obj
 def codechecker(east):
@@ -225,4 +392,6 @@ def codechecker(east):
 codechecker.add_command(check)
 codechecker.add_command(example_config)
 codechecker.add_command(fixit)
-# codechecker.add_command(store)
+codechecker.add_command(store)
+codechecker.add_command(bypass)
+codechecker.add_command(servdiff)

commit a83b1508306a8a97710671397485c9972f4a0aaf

Fix tests that broke in 81bcb75ee
Note to myself: If you use this function in some other place, make sure that you
patch it in the test files, otherwise pytest thinks that is the mocked
call that it should test against.

src/east/helper_functions.py
@@ -359,6 +359,10 @@ def get_git_version(east):
     """
     Return output from git describe command, see help string of release function for
     more information.
+
+    Note to myself: If you use this function in some other place, make sure that you
+    patch it in the test files, otherwise pytest thinks that is the mocked call that it
+    should test against.
     """
     result = east.run(
         "git describe --tags --always --long --dirty=+", silent=True, return_output=True

src/east/workspace_commands/codechecker_helpers.py
@@ -267,7 +267,8 @@ def create_codecheckerfile(east, board, build_type, build_dir, source_dir):
     """
 
     # Create a file in the build folder that contains the name of the artefact
-    filename = os.path.join(build_dir if build_dir else "build", "codecheckerfile.json")
+    build_dir = build_dir if build_dir else "build"
+    filename = os.path.join(build_dir, "codecheckerfile.json")
     name = os.path.basename(source_dir if source_dir else east.cwd)
     version = get_git_version(east)
 
@@ -277,6 +278,8 @@ def create_codecheckerfile(east, board, build_type, build_dir, source_dir):
         "build_type": build_type,
         "version": version,
     }
+    if not os.path.exists(build_dir):
+        os.makedirs(build_dir)
 
     with open(filename, "w") as f:
         json.dump(data, f)

tests/helpers.py
@@ -314,6 +314,10 @@ def helper_test_against_west_run(
         "east.workspace_commands.release_commands.get_git_version",
         return_value={"tag": "v1.0.0.", "hash": ""},
     )
+    mocker.patch(
+        "east.workspace_commands.codechecker_helpers.get_git_version",
+        return_value={"tag": "v1.0.0.", "hash": ""},
+    )
 
     monkeypatch.chdir(path)
     mocker.patch(