IRNAS/irnas-east-software

commit cbaad86562e838eb48183f9490ccf0ebdb65fa56

feat: determine the speed option from build dir in east util connect

src/east/helper_functions.py
@@ -402,15 +402,14 @@ def get_git_version(east):
     east.exit()
 
 
-def get_device(runner_yaml_content):
-    """Extract device flag from runner.yaml content."""
+def get_jlink_param(runner_yaml_content, jlink_param):
+    """Extract value for a given jlink param from runner.yaml content."""
     try:
         jlink_args = runner_yaml_content["args"]["jlink"]
-        device = next(filter(lambda e: "--device" in e, jlink_args))
-        return device.split("=")[1]
+        value = next(filter(lambda e: jlink_param in e, jlink_args))
+        return value.split("=")[1]
     except (KeyError, StopIteration):
         return None
-
 
 def find_app_build_dir(build_dir):
     """Find the build directory of the app."""
@@ -438,7 +437,7 @@ def get_device_in_runner_yaml(build_dir):
     with open(os.path.join(zephyr_dir, "runners.yaml")) as f:
         runners_yaml = yaml.safe_load(f)
 
-    device = get_device(runners_yaml)
+    device = get_jlink_param(runners_yaml, "--device")
 
     if not device:
         raise Exception(
@@ -447,6 +446,18 @@ def get_device_in_runner_yaml(build_dir):
         )
 
     return device
+
+def get_jlink_speed_in_runner_yaml(build_dir):
+    """Returns speed flag for JLink runner from runners.yaml.
+
+    If it is not found, None is returned, so caller should handle that case.
+    """
+    zephyr_dir = os.path.join(find_app_build_dir(build_dir), "zephyr")
+
+    with open(os.path.join(zephyr_dir, "runners.yaml")) as f:
+        runners_yaml = yaml.safe_load(f)
+
+    return get_jlink_param(runners_yaml, "--speed")
 
 
 def get_cortex_debug_params(build_dir):

src/east/system_commands/jlink_commands.py
@@ -1,7 +1,7 @@
 import click
 
 from ..east_context import east_command_settings
-from ..helper_functions import get_device_in_runner_yaml
+from ..helper_functions import get_device_in_runner_yaml, get_jlink_speed_in_runner_yaml
 
 
 def no_jlink_tool_msg(tool):
@@ -57,10 +57,9 @@ def fmt_runner_error_msg(exception_msg):
     "-s",
     "--speed",
     type=str,
-    default=4000,
     help=(
         "Set the connection speed, can be a number, 'auto' or 'adaptive'. "
-        "Default: '4000'."
+        "If not given, east tries to infer the speed by looking into --build-dir."
     ),
 )
 @click.pass_obj
@@ -75,7 +74,7 @@ def connect(east, device, dev_id, rtt_port, speed, build_dir):
         east.print(no_jlink_tool_msg("JLinkExe"), highlight=False)
         east.exit()
 
-    cmd = f"JLinkExe -AutoConnect 1 -Speed {speed} -If SWD -RTTTelnetPort {rtt_port} "
+    cmd = f"JLinkExe -AutoConnect 1 -If SWD -RTTTelnetPort {rtt_port} "
 
     if dev_id:
         cmd += f"-USB {dev_id} "
@@ -83,12 +82,21 @@ def connect(east, device, dev_id, rtt_port, speed, build_dir):
     if not device:
         try:
             device = get_device_in_runner_yaml(build_dir)
+            cmd += f"-Device {device} "
         except Exception as msg:
             east.print(fmt_runner_error_msg(msg), highlight=False)
             east.exit()
 
-    if device:
-        cmd += f"-Device {device} "
+    if not speed:
+        speed = get_jlink_speed_in_runner_yaml(build_dir)
+        if not speed:
+            speed = 4000
+            east.print(
+                f"No --speed param found in runner.yml, falling back to {speed}",
+                highlight=False,
+            )
+
+        cmd += f"-Speed {speed} "
 
     east.run(cmd)
 

commit e23526441b14df9d58fe36359e711c93fed57f17

fix: correct east util cortex-debug that got broken in cbaad865
The culprit was that get_device didn't exist anymore after cbaad865 and
it should be replaced by the get_device_in_runner_yaml but it wasn't

The rest of the changes refactor surrounding code.

src/east/helper_functions.py
@@ -427,15 +427,25 @@ def does_project_use_sysbuild(build_dir):
     return os.path.isfile(os.path.join(build_dir, "domains.yaml"))
 
 
+def get_runner_yaml(build_dir):
+    """Returns runner.yaml content as dict."""
+    if not os.path.isdir(build_dir):
+        raise Exception(f"Build directory {build_dir} not found")
+
+    zephyr_dir = os.path.join(find_app_build_dir(build_dir), "zephyr")
+
+    with open(os.path.join(zephyr_dir, "runners.yaml")) as f:
+        runners_yaml = yaml.safe_load(f)
+
+    return runners_yaml
+
+
 def get_device_in_runner_yaml(build_dir):
     """Returns device flag for JLink runner from runners.yaml.
 
     If it is not possible to do that it will raise an exception.
     """
-    zephyr_dir = os.path.join(find_app_build_dir(build_dir), "zephyr")
-
-    with open(os.path.join(zephyr_dir, "runners.yaml")) as f:
-        runners_yaml = yaml.safe_load(f)
+    runners_yaml = get_runner_yaml(build_dir)
 
     device = get_jlink_param(runners_yaml, "--device")
 
@@ -453,12 +463,7 @@ def get_jlink_speed_in_runner_yaml(build_dir):
 
     If it is not found, None is returned, so caller should handle that case.
     """
-    zephyr_dir = os.path.join(find_app_build_dir(build_dir), "zephyr")
-
-    with open(os.path.join(zephyr_dir, "runners.yaml")) as f:
-        runners_yaml = yaml.safe_load(f)
-
-    return get_jlink_param(runners_yaml, "--speed")
+    return get_jlink_param(get_runner_yaml(build_dir), "--speed")
 
 
 def get_cortex_debug_params(build_dir):
@@ -469,30 +474,21 @@ def get_cortex_debug_params(build_dir):
     Any failure to read something will result in a raised exception so this should be
     called inside the try block.
     """
-    if not os.path.isdir(build_dir):
-        raise Exception(f"Build directory {build_dir} not found")
-
     zephyr_dir = os.path.join(find_app_build_dir(build_dir), "zephyr")
-
-    runners_file = os.path.join(zephyr_dir, "runners.yaml")
+    runners_file_path = os.path.join(zephyr_dir, "runners.yaml")
     elf_file = os.path.join(zephyr_dir, "zephyr.elf")
     elf_file = os.path.realpath(elf_file)
 
-    with open(runners_file) as f:
-        runner_cfg = yaml.safe_load(f)
+    runners_yaml = get_runner_yaml(build_dir)
+    device = get_device_in_runner_yaml(build_dir)
 
-    device = get_device(runner_cfg)
-
-    if not device:
-        raise Exception(f"Can't find JLink device in the {runners_file} file")
-
-    if "gdb" not in runner_cfg["config"]:
-        raise Exception(f"Can't find gdb path in the {runners_file} file")
+    if "gdb" not in runners_yaml["config"]:
+        raise Exception(f"Can't find gdb path in the {runners_file_path} file")
 
     if not os.path.isfile(elf_file):
         raise Exception(f"Elf file {elf_file} not found")
 
-    return device, runner_cfg["config"]["gdb"], elf_file
+    return device, runners_yaml["config"]["gdb"], elf_file
 
 
 def determine_svd_file(east, device):